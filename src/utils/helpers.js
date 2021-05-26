c8d3bc7e9177ff23ac8d17a1df5f562d2ed9bbfc
Szymczyk, Piotr <piotr.szymczyk@accenture.com>
2021-05-26 19:37:23 +0200

    regular expression fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,6 +1,11 @@
 export const isURL = (str) => {
-    const pattern = new RegExp('^(https?:\/\/)?((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{1,}|((\d{1,3}\.){3}\d{1,3}))(\:\d+)?(\/[-a-z\d%_.~+]*)*(\?[;&a-z\d%_.~+=-]*)?(\#[-a-z\d_]*)?$'); // fragment locator
-    return !! pattern.test(str);
-  }
+  const pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
 
   
