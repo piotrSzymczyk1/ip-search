2cec17995206fea2a3c2ece3df3feac60796ed8e
Szymczyk, Piotr <piotr.szymczyk@accenture.com>
2021-05-26 18:45:44 +0200

    https added

--- src/services/apiEndpoints.js
+++ src/services/apiEndpoints.js
@@ -1,7 +1,7 @@
 export const ipStackApiKey = 'b84f2f3cec866ad70f079e31ffdd46dc'
 const API_ENDPOINTS = {
-  IP_LOCATION_SEARCH: `http://api.ipstack.com/check?access_key=${ipStackApiKey}`,
-  IP_URL_SEARCH: `http://api.ipstack.com/`
+  IP_LOCATION_SEARCH: `https://api.ipstack.com/check?access_key=${ipStackApiKey}`,
+  IP_URL_SEARCH: `https://api.ipstack.com/`
 };
 
 export default API_ENDPOINTS
